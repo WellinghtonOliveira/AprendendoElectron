e807253239385497586f1677b16c3bdbd9ab6c2a
Wellinghton O. <wellinghtonbatistahotmail@gmail.com>
2025-06-22 11:50:26 -0300

    Update forge.config.js

--- forge.config.js
+++ forge.config.js
@@ -7,8 +7,8 @@ module.exports = {
       name: '@electron-forge/publisher-github',
       config: {
         repository: {
-          owner: 'github-user-name',
-          name: 'github-repo-name'
+          owner: 'WellinghtonOliveira',
+          name: 'AprendendoElectron'
         },
         prerelease: false,
         draft: true
